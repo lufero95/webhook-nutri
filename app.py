3ce46ad023585a08ef40459d9780e35d949970b9
lufero95 <lucasfeijorodrigues@gmail.com>
2026-03-18 16:00:16 -0300

    app.py

--- app.py
+++ app.py
@@ -1,51 +1,24 @@
-from flask import Flask, request
-import smtplib
-from email.mime.text import MIMEText
-
-app = Flask(__name__)
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-
-    data = request.json
-
-    nome = data.get("nome")
-    email = data.get("email")
-
-    print("Dados recebidos:", data)
-
-    enviar_email(nome, email)
-
-    return "ok"
-
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 def enviar_email(nome, email):
+    message = Mail(
+        from_email='SEUEMAIL@gmail.com',
+        to_emails=email,
+        subject='Seu link de pagamento',
+        html_content=f"""
+        <p>Olá {nome},</p>
+        <p>Clique abaixo para pagar:</p>
+        <a href="https://invoice.infinitepay.io/feijonut/7NgEpgMrIJ">
+        PAGAR AGORA
+        </a>
+        """
+    )
 
-    link_pagamento = "https://invoice.infinitepay.io/feijonut/7NgEpgMrIJ"
-
-    mensagem = f"""
-Olá {nome}!
-
-Seu plano alimentar personalizado está quase pronto.
-
-Para liberar seu acesso basta realizar o pagamento abaixo:
-
-{link_pagamento}
-
-Assim que confirmado você receberá o protocolo completo.
-
-Abraços!
-"""
-
-    msg = MIMEText(mensagem)
-    msg['Subject'] = "Liberação do protocolo"
-    msg['From'] = "lucasfeijorodrigues@gmail.com"
-    msg['To'] = email
-
-    servidor = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    servidor.login("lucasfeijorodrigues@gmail.com", "uxtn eqhe qxcc cgpu")
-    servidor.send_message(msg)
-    servidor.quit()
-
-
-app.run(host="0.0.0.0", port=10000)
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print("Email enviado!", response.status_code)
+    except Exception as e:
+        print("Erro ao enviar email:", e)
