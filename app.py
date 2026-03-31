bd5b9da6c5bf360b40c49600d9b26957ea8692ae
lufero95 <lucasfeijorodrigues@gmail.com>
2026-03-31 14:20:53 -0300

    app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def criar_pagamento(nome, email):
         "items": [
             {
                 "quantity": 1,
-                "price": 010,
+                "price": 0010,
                 "description": "Produto 7D Desincha"
             }
         ]
